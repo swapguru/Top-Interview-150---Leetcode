3eea21d8dd21b1aab209e3209b68ed6cc1001f17
Swapnil Patil <patil.k.swapnil@gmail.com>
2024-03-01 03:57:38 -0330

    Adds solution to Jump Game

---
+++ jump_game.rb
@@ -0,0 +1,36 @@
+=begin
+
+You are given an integer array nums. You are initially positioned at the array's first index, and each element in the array represents your maximum jump length at that position.
+
+Return true if you can reach the last index, or false otherwise.
+
+Example 1:
+
+Input: nums = [2,3,1,1,4]
+Output: true
+Explanation: Jump 1 step from index 0 to 1, then 3 steps to the last index.
+Example 2:
+
+Input: nums = [3,2,1,0,4]
+Output: false
+Explanation: You will always arrive at index 3 no matter what. Its maximum jump length is 0, which makes it impossible to reach the last index.
+
+Constraints:
+
+1 <= nums.length <= 104
+0 <= nums[i] <= 105
+
+=end
+
+# @param {Integer[]} nums
+# @return {Boolean}
+def can_jump(nums)
+  target = nums.length - 1
+
+  (nums.length - 1).downto(0).each do |index|
+      if index + nums[index] >= target
+          target = index
+      end
+  end
+  target == 0
+end
