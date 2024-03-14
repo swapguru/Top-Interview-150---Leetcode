39f12d7e3c76cacb0b47db6fbff40a09fa0ce82e
Swapnil Patil <patil.k.swapnil@gmail.com>
2024-03-14 17:04:14 -0230

    Adds solution for Find Index of the First Occurence in a String

---
+++ first_occurence.rb
@@ -0,0 +1,29 @@
+=begin
+Given two strings needle and haystack, return the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack.
+
+Example 1:
+
+Input: haystack = "sadbutsad", needle = "sad"
+Output: 0
+Explanation: "sad" occurs at index 0 and 6.
+The first occurrence is at index 0, so we return 0.
+Example 2:
+
+Input: haystack = "leetcode", needle = "leeto"
+Output: -1
+Explanation: "leeto" did not occur in "leetcode", so we return -1.
+
+Constraints:
+
+1 <= haystack.length, needle.length <= 104
+haystack and needle consist of only lowercase English characters.
+
+=end
+
+# @param {String} haystack
+# @param {String} needle
+# @return {Integer}
+def str_str(haystack, needle)
+  index = haystack.index(needle)
+  index.nil? ? -1 : index
+end
