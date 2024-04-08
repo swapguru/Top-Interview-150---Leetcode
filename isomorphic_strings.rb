244d26c022658411a126dd25caa0a7b969d8f800
Swapnil Patil <patil.k.swapnil@gmail.com>
2024-04-08 12:40:43 -0230

    Adds solution for Isomorphic Strings

---
+++ isomorphic_strings.rb
@@ -0,0 +1,33 @@
+=begin
+Given two strings s and t, determine if they are isomorphic.
+
+Two strings s and t are isomorphic if the characters in s can be replaced to get t.
+
+All occurrences of a character must be replaced with another character while preserving the order of characters. No two characters may map to the same character, but a character may map to itself.
+
+Example 1:
+
+Input: s = "egg", t = "add"
+Output: true
+Example 2:
+
+Input: s = "foo", t = "bar"
+Output: false
+Example 3:
+
+Input: s = "paper", t = "title"
+Output: true
+
+Constraints:
+
+1 <= s.length <= 5 * 104
+t.length == s.length
+s and t consist of any valid ascii character.
+=end
+
+# @param {String} s
+# @param {String} t
+# @return {Boolean}
+def is_isomorphic(s, t)
+  s.chars.map { |c| s.index(c) }  == t.chars.map { |c| t.index(c) }
+end
