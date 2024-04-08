861b9cf871e10b2e43fe0af0c57bf88f4897a18b
Swapnil Patil <patil.k.swapnil@gmail.com>
2024-04-08 12:51:11 -0230

    Adds solution for Word Pattern

---
+++ word_pattern.rb
@@ -0,0 +1,45 @@
+=begin
+Given a pattern and a string s, find if s follows the same pattern.
+
+Here follow means a full match, such that there is a bijection between a letter in pattern and a non-empty word in s.
+
+Example 1:
+
+Input: pattern = "abba", s = "dog cat cat dog"
+Output: true
+Example 2:
+
+Input: pattern = "abba", s = "dog cat cat fish"
+Output: false
+Example 3:
+
+Input: pattern = "aaaa", s = "dog cat cat dog"
+Output: false
+
+Constraints:
+
+1 <= pattern.length <= 300
+pattern contains only lower-case English letters.
+1 <= s.length <= 3000
+s contains only lowercase English letters and spaces ' '.
+s does not contain any leading or trailing spaces.
+All the words in s are separated by a single space.
+
+=end
+
+# @param {String} pattern
+# @param {String} s
+# @return {Boolean}
+def word_pattern(pattern, s)
+  s_ary = s.split(" ")
+  return false if pattern.size != s_ary.size
+
+  hash = {}
+  pattern.split("").each_with_index do |char, i|
+      if !hash[char]
+          hash[char] = s_ary[i]
+      end
+      return false if hash[char] != s_ary[i]
+  end
+  hash.values == hash.values.uniq
+end
